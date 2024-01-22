f5d8c312de0a757de01fff2ea834a6ff4a406ebe
fsiilva <111534447+fsiilva@users.noreply.github.com>
2024-01-22 12:00:56 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela (tag, texto){
 
 function mensagemInicial() {
     exibirTextoNaTela(`h1`, `Jogo do numero secreto`);
-    exibirTextoNaTela(`p`,`Escolha u numero entre 1 e 10`);   
+    exibirTextoNaTela(`p`,`Escolha um numero entre 1 e 10`);   
 }
 
 mensagemInicial();
